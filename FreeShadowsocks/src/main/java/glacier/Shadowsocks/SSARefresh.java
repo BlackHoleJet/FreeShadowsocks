1ab694b8d71a87987108b3862ca0301ac686ae62
少湖 <shuifeng.lsf@alibaba-inc.com>
2015-08-10 15:00:29 +0800

    remove main log

--- FreeShadowsocks/src/main/java/glacier/Shadowsocks/SSARefresh.java
+++ FreeShadowsocks/src/main/java/glacier/Shadowsocks/SSARefresh.java
@@ -3,12 +3,8 @@ package glacier.Shadowsocks;
 import it.sauronsoftware.junique.AlreadyLockedException;
 import it.sauronsoftware.junique.JUnique;
 
-import org.apache.log4j.Logger;
-
 public class SSARefresh
 {
-    private static Logger logger = Logger.getLogger(SSARefresh.class.getName());
-    
     public static void main(String[] args) throws Exception
     {
         String appName = "shadowsocks_auto_refresher";
